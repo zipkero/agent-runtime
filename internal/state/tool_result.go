18abdb4f6ee22f4a4ca5a74c5bbc77e10818c9a5
moonyoung <kero@triptopaz.com>
2026-03-27 14:24:28 +0900

    ToolResult struct 추가

---
+++ internal/state/tool_result.go
@@ -0,0 +1,10 @@
+package state
+
+// ToolResult 는 Tool 실행 결과를 담는 구조체다.
+// IsError 가 true 일 때 ErrMsg 에 에러 내용이 담긴다.
+type ToolResult struct {
+	ToolName string
+	Output   string
+	IsError  bool
+	ErrMsg   string
+}
